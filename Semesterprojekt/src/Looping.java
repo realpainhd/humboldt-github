a12343a997fd1ea100f70089e0f0cf428a80a3f0
realpainhd <149602543+realpainhd@users.noreply.github.com>
2023-11-22 15:55:01 +0100

    Kontroll Methode implementiert

--- Semesterprojekt/src/Looping.java
+++ Semesterprojekt/src/Looping.java
@@ -2,15 +2,16 @@
 public class Looping {
 
 	public static void main(String[] args) {
-		int N = Integer.parseInt(args[0]);
-		int R = reverse(N);
+		long N = Integer.parseInt(args[0]);
+		long R = reverse(N);
 		System.out.println(N);
 		System.out.println(R);
-
+		long t = N + R;
+		System.out.println("Ist die zahl ein Palindrom?: " + checkPal(t));
 	}
 
-	public static int reverse(int num) {
-		String s1 = Integer.toString(num);
+	public static long reverse(long num) {
+		String s1 = Long.toString(num);
 		String s2 = "";
 		for (int i = s1.length() - 1; i >= 0; i--) {
 			s2 += s1.charAt(i);
@@ -21,8 +22,12 @@ public class Looping {
 
 	}
 
-	public static boolean checkPal(int n) {
-
+	public static boolean checkPal(long t) {
+		long x = t;
+		long y = reverse(t);
+		if (x == y) {
+			return true;
+		}
 		return false;
 	}
 
